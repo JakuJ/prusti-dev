cba3a6a8f03f18dd059e10e204f7226b6d6e24ab
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2023-02-21 18:50:30 +0100

    Add Lithium back end

--- prusti-viper/src/encoder/errors/error_manager.rs
+++ prusti-viper/src/encoder/errors/error_manager.rs
@@ -4,22 +4,18 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use std::fmt::Debug;
-
-use vir_crate::polymorphic::Position;
-use rustc_hash::FxHashMap;
-use prusti_rustc_interface::span::source_map::SourceMap;
-use prusti_rustc_interface::errors::MultiSpan;
-use viper::VerificationError;
-use prusti_interface::PrustiError;
-use log::debug;
 use super::PositionManager;
-use prusti_interface::data::ProcedureDefId;
+use backend_common::VerificationError;
+use log::debug;
+use prusti_interface::{data::ProcedureDefId, PrustiError};
+use prusti_rustc_interface::{errors::MultiSpan, span::source_map::SourceMap};
+use rustc_hash::FxHashMap;
+use vir_crate::polymorphic::Position;
 
-const ASSERTION_TIMEOUT_HELP_MESSAGE: &str = "This could be caused by too small assertion timeout. \
+const ASSERTION_TIMEOUT_HELP_MESSAGE: &str =
+    "This could be caused by too small assertion timeout. \
 Try increasing it by setting the configuration parameter \
 ASSERT_TIMEOUT to a larger value.";
-
 
 /// The cause of a panic!()
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
@@ -227,7 +223,8 @@ impl<'tcx> ErrorManager<'tcx> {
         assert_ne!(pos, Position::default(), "Trying to register an error on a default position");
         if let Some(existing_error_ctxt) = self.error_contexts.get(&pos.id()) {
             debug_assert_eq!(
-                existing_error_ctxt, &error_ctxt,
+                existing_error_ctxt,
+                &error_ctxt,
                 "An existing error context would be overwritten.\n\
                 Position id: {}\n\
                 Existing error context: {:?}\n\
@@ -243,7 +240,11 @@ impl<'tcx> ErrorManager<'tcx> {
     /// Creates a new position with `error_ctxt` that is linked to `pos`. This
     /// method is used for setting the surrounding context position of an
     /// expression's position.
-    pub fn set_surrounding_error_context(&mut self, pos: Position, error_ctxt: ErrorCtxt) -> Position {
+    pub fn set_surrounding_error_context(
+        &mut self,
+        pos: Position,
+        error_ctxt: ErrorCtxt,
+    ) -> Position {
         let surrounding_position = self.duplicate_position(pos);
         self.set_error(surrounding_position, error_ctxt);
         self.inner_positions.insert(surrounding_position.id(), pos);
@@ -259,7 +260,9 @@ impl<'tcx> ErrorManager<'tcx> {
     }
 
     pub fn get_def_id(&self, ver_error: &VerificationError) -> Option<ProcedureDefId> {
-        ver_error.offending_pos_id.as_ref()
+        ver_error
+            .offending_pos_id
+            .as_ref()
             .and_then(|id| id.parse().ok())
             .and_then(|id| self.position_manager.def_id.get(&id).copied())
     }
@@ -267,41 +270,33 @@ impl<'tcx> ErrorManager<'tcx> {
     #[tracing::instrument(level = "debug", skip(self))]
     pub fn translate_verification_error(&self, ver_error: &VerificationError) -> PrustiError {
         let opt_pos_id: Option<u64> = match ver_error.offending_pos_id {
-            Some(ref viper_pos_id) => {
-                match viper_pos_id.parse() {
-                    Ok(pos_id) => Some(pos_id),
-                    Err(err) => {
-                        return PrustiError::internal(
-                            format!(
-                                "unexpected Viper position '{viper_pos_id}': {err}"
-                            ),
-                            MultiSpan::new()
-                        );
-                    }
+            Some(ref viper_pos_id) => match viper_pos_id.parse() {
+                Ok(pos_id) => Some(pos_id),
+                Err(err) => {
+                    return PrustiError::internal(
+                        format!("unexpected Viper position '{viper_pos_id}': {err}"),
+                        MultiSpan::new(),
+                    );
                 }
-            }
-            None => None
+            },
+            None => None,
         };
         let opt_reason_pos_id: Option<u64> = match ver_error.reason_pos_id {
-            Some(ref viper_reason_pos_id) => {
-                match viper_reason_pos_id.parse() {
-                    Ok(reason_pos_id) => Some(reason_pos_id),
-                    Err(err) => {
-                        return PrustiError::internal(
-                            format!(
-                                "unexpected Viper reason position '{viper_reason_pos_id}': {err}"
-                            ),
-                            MultiSpan::new()
-                        );
-                    }
+            Some(ref viper_reason_pos_id) => match viper_reason_pos_id.parse() {
+                Ok(reason_pos_id) => Some(reason_pos_id),
+                Err(err) => {
+                    return PrustiError::internal(
+                        format!("unexpected Viper reason position '{viper_reason_pos_id}': {err}"),
+                        MultiSpan::new(),
+                    );
                 }
-            }
-            None => None
+            },
+            None => None,
         };
 
-        let opt_error_ctxts = opt_pos_id
-            .and_then(|pos_id| self.error_contexts.get(&pos_id));
-        let opt_error_span = opt_pos_id.and_then(|pos_id| self.position_manager.source_span.get(&pos_id));
+        let opt_error_ctxts = opt_pos_id.and_then(|pos_id| self.error_contexts.get(&pos_id));
+        let opt_error_span =
+            opt_pos_id.and_then(|pos_id| self.position_manager.source_span.get(&pos_id));
         let opt_cause_span = opt_reason_pos_id.and_then(|reason_pos_id| {
             let res = self.position_manager.source_span.get(&reason_pos_id);
             if res.is_none() {
@@ -317,7 +312,7 @@ impl<'tcx> ErrorManager<'tcx> {
                 ver_error,
                 error_span,
                 opt_cause_span,
-                error_ctxt
+                error_ctxt,
             )
         } else {
             debug!("Unregistered verification error: {:?}", ver_error);
@@ -328,24 +323,22 @@ impl<'tcx> ErrorManager<'tcx> {
             };
 
             match opt_pos_id {
-                Some(ref pos_id) => {
-                    PrustiError::internal(
-                        format!(
-                            "unregistered verification error: [{}; {}] {}",
-                            ver_error.full_id, pos_id, ver_error.message
-                        ),
-                        error_span
-                    ).set_help(ASSERTION_TIMEOUT_HELP_MESSAGE)
-                }
-                None => {
-                    PrustiError::internal(
-                        format!(
-                            "unregistered verification error: [{}] {}",
-                            ver_error.full_id, ver_error.message
-                        ),
-                        error_span
-                    ).set_help(ASSERTION_TIMEOUT_HELP_MESSAGE)
-                }
+                Some(ref pos_id) => PrustiError::internal(
+                    format!(
+                        "unregistered verification error: [{}; {}] {}",
+                        ver_error.full_id, pos_id, ver_error.message
+                    ),
+                    error_span,
+                )
+                .set_help(ASSERTION_TIMEOUT_HELP_MESSAGE),
+                None => PrustiError::internal(
+                    format!(
+                        "unregistered verification error: [{}] {}",
+                        ver_error.full_id, ver_error.message
+                    ),
+                    error_span,
+                )
+                .set_help(ASSERTION_TIMEOUT_HELP_MESSAGE),
             }
         }
     }
